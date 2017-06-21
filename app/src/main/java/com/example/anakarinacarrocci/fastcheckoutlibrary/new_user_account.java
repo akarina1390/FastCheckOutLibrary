bfb458536fc4d7edb96fd079ba4ffaf6e848e5e8
akarina1390 <akarina1390@gmail.com>
2017-06-20 22:53:12 -0600

    Add new panels

---
+++ app/src/main/java/com/example/anakarinacarrocci/fastcheckoutlibrary/new_user_account.java
@@ -0,0 +1,13 @@
+package com.example.anakarinacarrocci.fastcheckoutlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class new_user_account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user_account);
+    }
+}
