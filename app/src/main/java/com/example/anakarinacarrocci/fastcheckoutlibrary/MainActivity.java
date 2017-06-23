ef931182091d6b52cf8f8b0c81e1836c445cc7cc
Brandon Sieu <brandon.sieu@ucalgary.ca>
2017-06-23 16:43:22 -0600

    Added functionality to login and new user button

--- app/src/main/java/com/example/anakarinacarrocci/fastcheckoutlibrary/MainActivity.java
+++ app/src/main/java/com/example/anakarinacarrocci/fastcheckoutlibrary/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.anakarinacarrocci.fastcheckoutlibrary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signIn = (Button) findViewById(R.id.buttonSignIn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), UserSearchMainMenu.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        Button userRegister = (Button) findViewById(R.id.buttonNewUser);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), NewUserAccount.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
+
 }
