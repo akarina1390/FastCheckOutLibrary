edae7936d319b9d5e5ed94378cde47a265640fdd
akarina1390 <akarina1390@gmail.com>
2017-06-22 19:18:22 -0600

    User Interface Android App

---
+++ app/src/main/java/com/example/anakarinacarrocci/fastcheckoutlibrary/SearchByLibrary.java
@@ -0,0 +1,13 @@
+package com.example.anakarinacarrocci.fastcheckoutlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchByLibrary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_by_library);
+    }
+}
--- app/src/main/java/com/example/anakarinacarrocci/fastcheckoutlibrary/UserSearchMainMenu.java
+++ app/src/main/java/com/example/anakarinacarrocci/fastcheckoutlibrary/UserSearchMainMenu.java
@@ -3,11 +3,11 @@ package com.example.anakarinacarrocci.fastcheckoutlibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BookSearch extends AppCompatActivity {
+public class UserSearchMainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_search);
+        setContentView(R.layout.activity_user_search_main_menu);
     }
 }
